6f4bb93ef8357e31ed9c7bbd236e9260d632b035
ReijiAkkerman <reijiakkerman@gmail.com>
2025-09-01 16:53:49 +0000

    Сделал анимацию для кнопок управления списками

--- src/js/view/tools_listsControl__group.js
+++ src/js/view/tools_listsControl__group.js
@@ -28,5 +28,156 @@ class tools_listsControl__group {
 
 
 
-    //
+    static #animationInstance;
+    static #animationDuration = 200;
+    static #animationDelay = 200;
+    static #timingInfo = {
+        duration: 80,
+        fill: "forwards",
+        easing: "steps(2, end)",
+    };
+
+
+    
+    static #TIMEOUT_ID__ICON = false;
+    static #ICON_SHOWN = true;
+
+    static #TIMEOUT_ID__DESCRIPTION = false;
+    static #DESCRIPTION_SHOWN = false;
+
+
+
+
+
+    static showTip__personalLists() {
+        tools_listsControl__group.#hideIcon(tools_listsControl__group.#personalLists__icon);
+        tools_listsControl__group.#showDescription(tools_listsControl__group.#personalLists__description);
+    }
+
+    static hideTip__personalLists() {
+        tools_listsControl__group.#hideDescription(tools_listsControl__group.#personalLists__description);
+        tools_listsControl__group.#showIcon(tools_listsControl__group.#personalLists__icon);
+    }
+
+    static showTip__friendLists() {
+        tools_listsControl__group.#hideIcon(tools_listsControl__group.#friendLists__icon);
+        tools_listsControl__group.#showDescription(tools_listsControl__group.#friendLists__description);
+    }
+
+    static hideTip__friendLists() {
+        tools_listsControl__group.#hideDescription(tools_listsControl__group.#friendLists__description);
+        tools_listsControl__group.#showIcon(tools_listsControl__group.#friendLists__icon);
+    }
+
+    static showTip__groupLists() {
+        tools_listsControl__group.#hideIcon(tools_listsControl__group.#groupLists__icon);
+        tools_listsControl__group.#showDescription(tools_listsControl__group.#groupLists__description);
+    }
+
+    static hideTip__groupLists() {
+        tools_listsControl__group.#hideDescription(tools_listsControl__group.#groupLists__description);
+        tools_listsControl__group.#showIcon(tools_listsControl__group.#groupLists__icon);
+    }
+
+
+
+
+
+    static #hideIcon(icon) {
+        let animationInfo = [{opacity:0}];
+        let timingInfo = {
+            duration: tools_listsControl__group.#animationDuration,
+            fill: "forwards",
+            easing: "ease-in-out",
+        };
+        tools_listsControl__group.#animationInstance = icon.animate(animationInfo, timingInfo);
+        tools_listsControl__group.#TIMEOUT_ID__ICON = setTimeout(() => {
+            icon.style.display = "none";
+            icon.style.opacity = 0;
+            tools_listsControl__group.#SET_STATUS__ICON_HIDDEN();
+        }, tools_listsControl__group.#animationDelay);
+    }
+
+    static #showIcon(icon) {
+        let animationInfo = [{opacity:1}];
+        let timingInfo = {
+            duration: tools_listsControl__group.#animationDuration,
+            fill: "forwards",
+            easing: "ease-in-out",
+        };
+        if(tools_listsControl__group.#ICON_SHOWN === false) {
+            icon.style.display = "";
+            icon.animate(animationInfo, timingInfo);
+            icon.style.opacity = 1;
+            tools_listsControl__group.#SET_STATUS__ICON_SHOWN();
+        }
+        else {
+            tools_listsControl__group.#animationInstance.reverse();
+            tools_listsControl__group.#DECLINE_ICON_SHOWING();
+        }
+    }
+
+    static #showDescription(description) {
+        let animationInfo = [
+            {opacity:1},
+            {opacity:0},
+            {opacity:1},
+        ];
+        let timingInfo = {
+            duration: 80,
+            fill: "forwards",
+            easing: "steps(2, end)",
+        };
+        tools_listsControl__group.#TIMEOUT_ID__DESCRIPTION = setTimeout(() => {
+            description.style.display = "";
+            description.animate(animationInfo, timingInfo);
+            tools_listsControl__group.#SET_STATUS__DESCRIPTION_SHOWN();
+        }, tools_listsControl__group.#animationDelay);
+    }
+
+    static #hideDescription(description) {
+        if(tools_listsControl__group.#DESCRIPTION_SHOWN === true) {
+            description.style.display = "none";
+            tools_listsControl__group.#SET_STATUS__DESCRIPTION_HIDDEN();
+        }
+        else 
+            tools_listsControl__group.#DECLINE_DESCRIPTION_SHOWING();
+    }
+
+
+
+
+
+    static #SET_STATUS__ICON_HIDDEN() {
+        tools_listsControl__group.#ICON_SHOWN = false;
+    }
+
+    static #SET_STATUS__ICON_SHOWN() {
+        tools_listsControl__group.#ICON_SHOWN = true;
+    }
+
+    static #SET_STATUS__DESCRIPTION_SHOWN() {
+        tools_listsControl__group.#DESCRIPTION_SHOWN = true;
+    }
+
+    static #SET_STATUS__DESCRIPTION_HIDDEN() {
+        tools_listsControl__group.#DESCRIPTION_SHOWN = false;
+    }
+
+    static #DECLINE_ICON_SHOWING() {
+        clearTimeout(tools_listsControl__group.#TIMEOUT_ID__ICON);
+    }
+
+    static #DECLINE_DESCRIPTION_SHOWING() {
+        clearTimeout(tools_listsControl__group.#TIMEOUT_ID__DESCRIPTION);
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    tools_listsControl__group.personalLists__button.addEventListener("mouseenter", tools_listsControl__group.showTip__personalLists);
+    tools_listsControl__group.personalLists__button.addEventListener("mouseleave", tools_listsControl__group.hideTip__personalLists);
+    tools_listsControl__group.friendLists__button.addEventListener("mouseenter", tools_listsControl__group.showTip__friendLists);
+    tools_listsControl__group.friendLists__button.addEventListener("mouseleave", tools_listsControl__group.hideTip__friendLists);
+    tools_listsControl__group.groupLists__button.addEventListener("mouseenter", tools_listsControl__group.showTip__groupLists);
+    tools_listsControl__group.groupLists__button.addEventListener("mouseleave", tools_listsControl__group.hideTip__groupLists);
+});
