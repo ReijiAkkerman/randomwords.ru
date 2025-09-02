ac5baf5a9b95be3d0c65be2c3fe22a0a47f57741
ReijiAkkerman <reijiakkerman@gmail.com>
2025-09-02 17:20:27 +0000

    Сделал плавный переход к категориям и обратно к группам по наведению мыши

--- src/js/view/tools_listsControl.js
+++ src/js/view/tools_listsControl.js
@@ -1,7 +1,5 @@
 class tools_listsControl {
     static #selectors = new Map([
-        ["Область управления списками", ".tools-listsControl"],
-
         ["Область выбора группы списков", ".tools-listsControl-group"],
         ["Область выбора категории списков", ".tools-listsControl-cath"],
     ]);
@@ -10,33 +8,23 @@ class tools_listsControl {
 
 
 
-    static listsControl = document.querySelector(tools_listsControl.#selectors.get("Область управления списками"));
-
-    static listsControl__group = document.querySelector(tools_listsControl.#selectors.get("Область выбора группы списков"));
-    static listsControl__cath = document.querySelector(tools_listsControl.#selectors.get("Область выбора категории списков"));
+    static groupSelection__area = document.querySelector(tools_listsControl.#selectors.get("Область выбора группы списков"));
+    static cathegorySelection__area = document.querySelector(tools_listsControl.#selectors.get("Область выбора категории списков"));
 
 
 
 
 
-    static showGroupArea() {
-        // tools_listsControl.listsControl.scrollLeft = 0;
-        let animationInfo = [{scrollLeft:1}];
-        let timingInfo = {
-            duration: 200,
-            fill: "forwards",
-            easing: "linear",
-        };
-        tools_listsControl.listsControl.animate(animationInfo, timingInfo);
+    static showGroupSelectionArea() {
+        tools_listsControl.groupSelection__area.scrollIntoView({behavior: "smooth", block: "start"});
     }
 
-    static showCathArea() {
-        let scrollLength = tools_listsControl.listsControl.scrollWidth - tools_listsControl.listsControl.clientWidth;
-        tools_listsControl.listsControl.scrollLeft = scrollLength;
+    static showCathegorySelectionArea() {
+        tools_listsControl.cathegorySelection__area.scrollIntoView({behavior: "smooth", block: "end"});
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    tools_listsControl.listsControl__group.addEventListener("mouseover", tools_listsControl.showGroupArea);
-    tools_listsControl.listsControl__cath.addEventListener("mouseover", tools_listsControl.showCathArea);
+    tools_listsControl.cathegorySelection__area.addEventListener("mouseenter", tools_listsControl.showCathegorySelectionArea);
+    tools_listsControl.groupSelection__area.addEventListener("mouseenter", tools_listsControl.showGroupSelectionArea);
 });
