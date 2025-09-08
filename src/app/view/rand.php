d82ffa118e04c77c2e6d971f305577c72c7c25da
ReijiAkkerman <reijiakkerman@gmail.com>
2025-09-08 15:04:20 +0000

    Сделал тестовую версию действий над категориями

--- src/app/view/rand.php
+++ src/app/view/rand.php
@@ -104,13 +104,13 @@
                     </button>
                 </div>
                 <div class="tools-listsControl__div tools-listsControl-cath">
-                    <button class="tools-listsControl-cath__button">
+                    <button class="tools-listsControl-cath__button tools__button_selectCathegory">
                         <svg style="display:none;" class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
                             <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"/>
                         </svg>
                         <pre class="tools-listsControl-cath__pre">Наименование группы</pre>
                     </button>
-                    <button class="tools-listsControl-cath__button">
+                    <button class="tools-listsControl-cath__button tools_button__selectSubcathegory">
                         <svg class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
                             <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"/>
                         </svg>
@@ -118,7 +118,42 @@
                     </button>
                 </div>
             </div>
-            <div class="tools__div tools-lists"></div>
+            <div class="tools__div tools-actionsBar">
+                <div class="tools-actionsBar-editing">
+                    <div class="tools-actionsBar-editing__div">
+                        <p class="tools-actionsBar-editing__p tools__p_nestingLevel">3</p>
+                    </div>
+                    <form class="tools-actionsBar-editing__form" action="#">
+                        <input class="tools-actionsBar-editing__input" type="text" placeholder="Программирование">
+                        <input type="hidden">
+                    </form>
+                    <button class=" tools-actionsBar-editing__button tools__button_selectCathColor"></button>
+                </div>
+                <div class="tools-actionsBar-confirmation">
+                    <button class="tools-actionsBar-confirmation__button tools__button_addNewCath">
+                        <svg class="tools-actionsBar-confirmation__svg" viewBox="0 0 24 24">
+                            <path d="M13 3C13 2.44772 12.5523 2 12 2C11.4477 2 11 2.44772 11 3V11H3C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H11V21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21V13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H13V3Z"/>
+                        </svg>
+                    </button>
+                    <button class="tools-actionsBar-confirmation__button tools__button_createNesting">
+                        <svg class="tools-actionsBar-confirmation__svg" viewBox="0 0 16 16" >
+                            <path fill-rule="evenodd" d="M11 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM6.025 7.5a5 5 0 1 1 0 1H4A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5h2.025zM11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"/>
+                        </svg>
+                    </button>
+                    <button class="tools-actionsBar-confirmation__button tools__button_saveCathChanges">
+                        <svg class="tools-actionsBar-confirmation__svg" viewBox="0 0 24 24">
+                            <path d="M16.765 2c1.187 0 1.363.06 1.51.168L21.662 4.7a.845.845 0 0 1 .339.677v15.78a.844.844 0 0 1-.844.844H2.844A.844.844 0 0 1 2 21.156V2.844A.844.844 0 0 1 2.844 2zM17 21v-7H7v7zM14 3v3h1V3zM7 3v6h10V3h-1v4h-3V3zM3 21h3v-8h12v8h3V5.452l-3-2.278v6.17a.769.769 0 0 1-.844.656H6.844A.769.769 0 0 1 6 9.344V3H3z"/>
+                        </svg>
+                    </button>
+                    <button class="tools-actionsBar-confirmation__button tools__button_deleteCath">
+                        <svg class="tools-actionsBar-confirmation__svg" viewBox="0 0 1024 1024">
+                            <path d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="tools__div tools-selectionBar"></div>
+            <div style="display:none;" class="tools__div tools-lists"></div>
         </main>
         <section class="settings"></section>
 
---
+++ src/js/view/tools_listsControl__cath.js
@@ -0,0 +1,26 @@
+class tools_listsControl__cath {
+    static #selectors = new Map([
+        ["Область выбора категории", ""],
+        ["Область выбора подкатегории", ""],
+
+        ["Панель выбора действия", ""],
+        ["Панель выбора категории", ""],
+        ["", ""],
+    ]);
+
+
+
+
+
+    static selectCath__area = document.querySelector(tools_listsControl__cath.#selectors.get("Область выбора категории"));
+    static selectSubcath__area = document.querySelector(tools_listsControl__cath.#selectors.get("Область выбора подкатегории"));
+
+    static selectAction__bar = document.querySelector(tools_listsControl__cath.#selectors.get("Панель выбора действия"));
+    static selectCath__bar = document.querySelector(tools_listsControl__cath.#selectors.get("Панель выбора категории"));
+
+
+
+
+
+    static 
+}
