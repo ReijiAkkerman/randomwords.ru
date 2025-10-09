6a11683494afa7a2ea8ff00cffa6c1efeaf3c5d7
ReijiAkkerman <reijiakkerman@gmail.com>
2025-10-09 15:17:02 +0300

    Добавил кнопку для показа дополнительных действий

--- src/app/view/rand.php
+++ src/app/view/rand.php
@@ -66,6 +66,11 @@
                         </svg>
                         <pre style="display:none;" class="tools-common__pre">Создать список</pre>
                     </button>
+                    <button class="tools-common__button tools__button_showAdditinalActions">
+                        <svg viewBox="0 0 1024 1024">
+                            <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"/>
+                        </svg>
+                    </button>
                 </div>
                 <div class="tools-common-search">
                     <button class="tools-common__button tools__button_search">
