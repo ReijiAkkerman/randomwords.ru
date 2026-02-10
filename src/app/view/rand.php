18a0ff00df680116dca86fe4faf894cbf0d6cb10
ReijiAkkerman <reijiakkerman@gmail.com>
2026-02-10 13:48:18 +0300

    Стилизовал область текущей выбранной группы и изменил ее html-структуру  On branch do_the_switching_between_group_and_cathegory  Changes to be committed: 	modified:   .gitignore 	modified:   src/app/view/rand.php 	modified:   src/css/rand/position.css 	modified:   src/css/rand/style.css 	modified:   src/js/view/tools/listsControl/tools_listsControl__cath.js 	modified:   src/scss/rand/position.scss 	modified:   src/scss/rand/style.scss

--- src/app/view/rand.php
+++ src/app/view/rand.php
@@ -136,18 +136,26 @@
                     </button>
                 </div>
                 <div class="tools-listsControl__div tools-listsControl-cath">
-                    <button class="tools-listsControl-cath__button tools__button_selectCathegory">
-                        <svg style="display:none;" class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
-                            <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"/>
-                        </svg>
-                        <pre class="tools-listsControl-cath__pre">Наименование группы</pre>
-                    </button>
-                    <button class="tools-listsControl-cath__button tools_button__selectSubcathegory">
-                        <svg class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
-                            <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"/>
-                        </svg>
-                        <pre style="display:none;" class="tools-listsControl-cath__pre">Наименование категории</pre>
-                    </button>
+                    <div style="display:none;" class="tools-listsControl-cath__div tools-listsConstrol-cath_currentCathegory">
+                        <button class="tools-listsControl-cath__button switcher">
+                            <svg class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
+                                <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"/>
+                            </svg>
+                        </button>
+                        <button class="tools-listsControl-cath__button">
+                            <pre class="tools-listsControl-cath__pre">Наименование категории</pre>
+                        </button>
+                    </div>
+                    <div class="tools-listsControl-cath__div tools-listsControl-cath_currentGroup">
+                        <button class="tools-listsControl-cath__button">
+                            <pre class="tools-listsControl-cath__pre">Наименование группы</pre>
+                        </button>
+                        <button class="tools-listsControl-cath__button switcher">
+                            <svg class="tools-listsControl-cath__svg" viewBox="0 0 1024 1024">
+                                <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"/>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
             <div class="tools__div tools-actionsArea">
--- src/js/view/tools/listsControl/tools_listsControl__cath.js
+++ src/js/view/tools/listsControl/tools_listsControl__cath.js
@@ -1,26 +1,8 @@
 class tools_listsControl__cath {
     static #selectors = new Map([
-        ["Область выбора категории", ""],
-        ["Область выбора подкатегории", ""],
-
-        ["Панель выбора действия", ""],
-        ["Панель выбора категории", ""],
-        ["", ""],
+        ["Кнопка выбранной группы", ".tools__button_selectCathegory"],
+        ["Кнопка выбранной категории", ".tools__button_selectSubcathegory"],
+        ["Кнопка для отображения выбранной группы", ""],
+        ["Кнопка для отображения выбранной категории", ""],
     ]);
-
-
-
-
-
-    static selectCath__area = document.querySelector(tools_listsControl__cath.#selectors.get("Область выбора категории"));
-    static selectSubcath__area = document.querySelector(tools_listsControl__cath.#selectors.get("Область выбора подкатегории"));
-
-    static selectAction__bar = document.querySelector(tools_listsControl__cath.#selectors.get("Панель выбора действия"));
-    static selectCath__bar = document.querySelector(tools_listsControl__cath.#selectors.get("Панель выбора категории"));
-
-
-
-
-
-    static 
 }
