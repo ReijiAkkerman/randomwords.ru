9cb5984cd4e3b42e974f7aa3bccee22d48407559
ReijiAkkerman <reijiakkerman@gmail.com>
2026-05-11 11:38:28 +0300

    Сделал дизайн списка записей
     Changes to be committed:
    	modified:   src/app/view/finances.php
    	modified:   src/css/finances/position.css
    	modified:   src/css/finances/style.css
    	modified:   src/scss/finances/position.scss
    	modified:   src/scss/finances/style.scss

--- src/app/view/finances.php
+++ src/app/view/finances.php
@@ -10,50 +10,70 @@
         <link rel="stylesheet" href="/src/css/finances/style.css">
     </head>
     <body>
-        <main class="entry">
-            <h1 class="entry-title">Создать запись</h1>
-            <form action="#" method="POST">
-                <h2 class="entry-paramName">Фонд</h2>
-                <select class="entry-param-fund" name="" id="">
-                    <option class="entry-param-fund__option" value="Повседневное">Повседневное</option>
-                </select>
-                <h2 class="entry-paramName">Категория</h2>
-                <select class="entry-param entry-param-cath" name="cathegory" id="">
-                    <option class="entry-param-cath__option" value="loss" selected>Убыток</option>
-                    <option class="entry-param-cath__option" value="income">Прибыль</option>
-                </select>
-                <h2 class="entry-paramName">Тип</h2>
-                <select class="entry-param entry-param-type" name="type" id="">
-                    <option class="entry-param-type__option" value="Еда" selected>Еда</option>
-                    <option class="entry-param-type__option" value="Проезд">Проезд</option>
-                </select>
-                <h2 class="entry-paramName">Пользователь</h2>
-                <div class="entry-param entry-param-users">
-                    <div class="entry-param-users-user">
-                        <input class="entry-param-users-user__input" type="checkbox" id="Я" value="Я" checked>
-                        <label class="entry-param-users-user__label" for="Я">Я</label>
-                    </div>
-                    <div class="entry-param-users-user">
-                        <input class="entry-param-users-user__input" type="checkbox" id="Катя" value="Катя">
-                        <label class="entry-param-users-user__label" for="Катя">Катя</label>
-                    </div>
+        <section class="entries">
+            <h1 class="entries-title">Наименование фонда</h1>
+            <div class="entries__div">
+                <div class="entries-list">
+
+                    <p class="entries-list-date">2020-12-12</p>
+                    <button class="entries-list-item">
+                        <div class="entries-list-item-userColors">
+                            <div style="background-color:#00f;"></div>
+                            <div style="background-color:#f00;"></div>
+                        </div>
+                        <div class="entries-list-item-mainInfo" style="background-color:#f002;">
+                            <p class="entries-list-item-mainInfo_type">Проезд</p>
+                            <div class="entries-list-item-mainInfo__div">
+                                <p class="entries-list-item-mainInfo_sum">1000<span class="entries-list-item-mainInfo_currency">&#8381</span></p>
+                                <p class="entries-list-item-mainInfo_time">13:53</p>
+                            </div>
+                        </div>
+                    </button>
+
                 </div>
-                <h2 class="entry-paramName">Основные параметры</h2>
-                <div class="entry-param">
-                    <div class="entry-param__div">
-                        <input class="entry-param-sum" name="sum" type="number" placeholder="Сумма">
-                        <select class="entry-param-currency" name="" id="">
-                            <option class="entry-param-currency__option" value="">&#8381</option>
-                            <option class="entry-param-currency__option" value="">&#36</option>
+                <div class="entries-entry" style="display:none;">
+                    <form action="#" method="POST">
+                        <h2 class="entries-entry-paramName">Фонд</h2>
+                        <select class="entries-entry-param-fund" name="" id="">
+                            <option class="entries-entry-param-fund__option" value="Повседневное">Повседневное</option>
                         </select>
-                    </div>
-                    <textarea class="entry-param-description" name="description" id="" placeholder="Описание"></textarea>
+                        <h2 class="entries-entry-paramName">Категория</h2>
+                        <select class="entries-entry-param entries-entry-param-cath" name="cathegory" id="">
+                            <option class="entries-entry-param-cath__option" value="loss" selected>Убыток</option>
+                            <option class="entries-entry-param-cath__option" value="income">Прибыль</option>
+                        </select>
+                        <h2 class="entries-entry-paramName">Тип</h2>
+                        <select class="entries-entry-param entries-entry-param-type" name="type" id="">
+                            <option class="entries-entry-param-type__option" value="Еда" selected>Еда</option>
+                            <option class="entries-entry-param-type__option" value="Проезд">Проезд</option>
+                        </select>
+                        <h2 class="entries-entry-paramName">Пользователь</h2>
+                        <div class="entries-entry-param entries-entry-param-users">
+                            <div class="entries-entry-param-users-user">
+                                <input class="entries-entry-param-users-user__input" type="checkbox" id="Я" value="Я" checked>
+                                <label class="entries-entry-param-users-user__label" for="Я">Я</label>
+                            </div>
+                            <div class="entries-entry-param-users-user">
+                                <input class="entries-entry-param-users-user__input" type="checkbox" id="Катя" value="Катя">
+                                <label class="entries-entry-param-users-user__label" for="Катя">Катя</label>
+                            </div>
+                        </div>
+                        <h2 class="entries-entry-paramName">Основные параметры</h2>
+                        <div class="entries-entry-param">
+                            <div class="entries-entry-param__div">
+                                <input class="entries-entry-param-sum" name="sum" type="number" placeholder="Сумма">
+                                <select class="entries-entry-param-currency" name="" id="">
+                                    <option class="entries-entry-param-currency__option" value="">&#8381</option>
+                                    <option class="entries-entry-param-currency__option" value="">&#36</option>
+                                </select>
+                            </div>
+                            <textarea class="entries-entry-param-description" name="description" id="" placeholder="Описание"></textarea>
+                        </div>
+                        <button class="entries-entry__button entries-entry__button_saveEntry">Сохранить</button>
+                    </form>
                 </div>
-                <button class="entry__button entry__button_saveEntry">Сохранить</button>
-            </form>
-        </main>
-        <section class="entries"></section>
-        <section class="filters"></section>
+            </div>
+        </section>
         <section class="funds"></section>
     </body>
 </html>
